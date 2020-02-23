0a75aae17e4daf2b8ece3fea6d681b4ab3937926
BlackberryBlues <56761052+BlackberryBlues@users.noreply.github.com>
2020-02-23 14:08:15 +0100

    Finalized version

--- Equalizer.py
+++ Equalizer.py
@@ -2,25 +2,38 @@ from tkinter import *
 import random as rn
 
 master = Tk()
-canvas = Canvas(master, width=1000, height=700)
+canvas = Canvas(master, width=700, height=500, bg='black')
 canvas.pack()
-
+vals = ['31', '62', '125', '250', '500', '1K', '2K', '4K', '8K', '16K', '[Hz]']
 floor = 400
 
+
 def block(x, y, floor):
-    canvas.create_rectangle(x, y, x + 40, floor, fill='green')
+    if y < 100:
+        canvas.create_rectangle(x, 200, x + 40, floor, fill='green')
+        canvas.create_rectangle(x, 100, x + 40, 200, fill='yellow')
+        canvas.create_rectangle(x, y, x + 40, 100, fill='red')
+    elif y < 200:
+        canvas.create_rectangle(x, 200, x + 40, floor, fill='green')
+        canvas.create_rectangle(x, y, x + 40, 200, fill='yellow')
+    else:
+        canvas.create_rectangle(x, y, x + 40, floor, fill='green')
+
 
 def plot():
     canvas.delete('all')
     for i in range(60,600,50):
-        block(i, rn.randint(10, 300),floor)
+        if i != 560:
+            block(i, rn.randint(10, 300),floor)
+        canvas.create_text(i + 20, 420, text=vals[i//50 - 1], font=(20), fill='green')
     canvas.update()
-    canvas.after(1000)
+    canvas.after(50)
 
+for i in range(20):
+    plot()
 
-
-
+'''
 Button(text='Try me', command=plot).pack()
+'''
 
 mainloop()
-
