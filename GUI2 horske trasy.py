1bf6c85bd57956299aa6f1c4ef5fe9175c6c4526
BlackberryBlues <56761052+BlackberryBlues@users.noreply.github.com>
2020-02-27 10:11:24 +0100

    Tkinter
    Z pisomky 02/27/2020

--- GUI2 horske trasy.py
+++ GUI2 horske trasy.py
@@ -1,40 +1,46 @@
 from tkinter import *
 
-def read():
-    """I dont know how to unpack the values from single line file"""
-    global canvas
-    filename = route.get()
-    with open(f'{filename}.txt', 'r') as f:
-        for line in f:
-            hs = line.split(',')
-            hs = [int(h.strip('\n')) for h in hs]
-        print(f'Najnizsi bod: {min(hs)}')
-        wid = master.winfo_width()
-        canvas = Canvas(height=400, width=wid)
-        canvas.pack()
-        canvas.delete('all')
-        floor = 300
-        plt = []
-        x = 10
-        k = wid/len(hs)
-        for h in hs:
-            plt.append(x + k)
-            plt.append(floor - (h / 10))
-            x += k
-        canvas.create_line(plt)
+def plot():
+    try:
+        with open(f'{fname.get()}.txt', 'r') as f:
+            try:
+                h = [int(i) for i in f.read().split(',')]
+            except:
+                print('Chyba pri nacitani suboru')               
+        h_min = min(h)
+        h_max = max(h)
+        print(f'Najmensia nadmorska vyska na useku trasy je: {h_min} m.n.m.')
+
+        k = 5
+        lauf = []
+        floor = 400
+        span = round(550 / len(h), 2)
+        for i in range(len(h)):
+            lauf.append(span * (i + 1))
+            lauf.append(floor - (h[i] / 10))
+            if h[i] == h_min:
+                mi = [span * (i + 1) - k, floor - (h[i] / 10) - k, span * (i + 1) + k,floor - (h[i] / 10) + k]
+            if h[i] == h_max:
+                mx = [span * (i + 1) - k, floor - (h[i] / 10) - k, span * (i + 1) + k,floor - (h[i] / 10) + k]
+        canvas.delete(ALL)
+        canvas.create_line(lauf)
+        canvas.create_oval(mi, fill='blue')
+        canvas.create_oval(mx, fill='red')
+        Label(text=f'Minimum (blue): {h_min}, Maximum (red): {h_max}').pack()
+        
+    except:
+                print('Neplatny nazov suboru')
 
 
 
 
 master = Tk()
-master.title('Turista v1.2')
-
-Label(text='Zadaj trasu, bez ".txt":').pack()
-route = Entry()
-route.pack()
-Button(text='Zobraz trasu!', command=read).pack()
-wid = master.winfo_width()
-
-
+master.title('Turista v1.0')
+Label(text='Zadaj nazov suboru s trasou [bez ".txt"; idealne csv]:').pack()
+fname = Entry()
+fname.pack()
+Button(text='Zobraz trasu', command=plot).pack()
+canvas = Canvas(height=500, width=600)
+canvas.pack()
 
 mainloop()
