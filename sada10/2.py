3c957aa281ebaaf342d394c184db0895ab1dc892
BlackberryBlues <56761052+BlackberryBlues@users.noreply.github.com>
2020-03-12 13:06:25 +0100

    Add files via upload

--- sada10/2.py
+++ sada10/2.py
@@ -1,19 +1,14 @@
+def format_min(minutes):
+    return f'{int(minutes)//60}:{int(minutes)%60}'
+
+def format_spd(km, time):
+    return f'{round(float(km)/int(time)*60, 2)}'
+
 with open('vylety.txt', 'r', encoding='utf-8') as f:
-    loc = []
-    l = 0
-    t = 0
+    print(f'Jednotlive ciele:')
     for line in f.readlines():
         *_, km, time, miesto = line.split()
-        loc.append(miesto)
-        l += float(km)
-        t += int(time)
-
-    print('Jednotlive ciele:')
-    for m in loc:
-        print(f'\t{m}')
-
-    print(f'Trvanie cesty: {t//60} hod. {t%60} min.')
-    print(f'Priemerna rychlost: {round(l/t*60, 2)} km/h')
+        print(f'\t{miesto}\t{format_min(time)}\t{format_spd(km,time)}')
 
 
 
